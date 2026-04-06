a4d3ef4408e1689ba10fbd5d89342a4c233df388
tim-syntax <nystim83@gmail.com>
2026-04-06 06:49:58 -0500

    make true git config for allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["expense-tracker-yrvh.onrender.com"],
+    allowedHosts: true,
   },
   preview: {
-    allowedHosts: ["expense-tracker-yrvh.onrender.com"],
+    allowedHosts: true,
   },
 });
